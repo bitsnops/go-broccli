9346b3aef4183ef4b2fdf0438d44b50c9e067637
Mikolaj Gasior <g@f0x.pl>
2023-12-27 22:22:25 +0100

    Refactor to v2 (#2)
    * WIP Refactor the code
    
    * Add tests for param
    
    * Add more tests
    
    * Add more tests, update docs etc.

--- cli.go
+++ cli.go
@@ -4,7 +4,6 @@ import (
 	"flag"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"os"
 	"path"
 	"reflect"
@@ -12,284 +11,63 @@ import (
 	"text/tabwriter"
 )
 
-// CLI is main CLI application definition. It has a name, description, author (which are used only when printing usage syntax), commands and pointers to File instances to which standard output or errors are printed (named respectively stdout and stderr).
+// CLI is main CLI application definition.
+// It has a name, description, author which are printed out to the screen in the usage syntax.
+// Each CLI have commands (represented by Cmd).  Optionally, it is possible to require environment
+// variables.
 type CLI struct {
 	name        string
 	desc        string
 	author      string
-	cmds        map[string]*CLICmd
-	envs        map[string]*CLIFlag
+	cmds        map[string]*Cmd
+	envVars     map[string]*param
 	parsedFlags map[string]string
 	parsedArgs  map[string]string
-	stdout      *os.File
-	stderr      *os.File
-	stdin       *os.File
 }
 
-// AttachCmd attaches instance of CLICmd to CLI.
-func (c *CLI) AttachCmd(cmd *CLICmd) {
-	n := cmd.name
-	if c.cmds == nil {
-		c.cmds = make(map[string]*CLICmd)
+// NewCLI returns pointer to a new CLI instance with specified name, description and author.  All these are used when
+// displaying syntax help.
+func NewCLI(n string, d string, a string) *CLI {
+	c := &CLI{
+		name:        n,
+		desc:        d,
+		author:      a,
+		cmds:        map[string]*Cmd{},
+		envVars:     map[string]*param{},
+		parsedFlags: map[string]string{},
+		parsedArgs:  map[string]string{},
 	}
-	c.cmds[n] = cmd
+	return c
 }
 
-// AttachEnv attaches instance of CLIFlag as a required env.
-func (c *CLI) AttachEnv(flg *CLIFlag) {
-	n := flg.name
-	if c.envs == nil {
-		c.envs = make(map[string]*CLIFlag)
+// AddCmd returns pointer to a new command with specified name, description and handler.  Handler is a function that
+// gets called when command is executed.
+// Additionally, there is a set of options that can be passed as arguments.  Search for cmdOption for more info.
+func (c *CLI) AddCmd(n string, d string, h func(cli *CLI) int, opts ...cmdOption) *Cmd {
+	c.cmds[n] = &Cmd{
+		name:    n,
+		desc:    d,
+		flags:   map[string]*param{},
+		args:    map[string]*param{},
+		envVars: map[string]*param{},
+		handler: h,
+		options: cmdOptions{},
 	}
-	c.envs[n] = flg
+	for _, o := range opts {
+		o(&(c.cmds[n].options))
+	}
+	return c.cmds[n]
 }
 
-// GetCmd returns instance of CLICmd of command k.
-func (c *CLI) GetCmd(k string) *CLICmd {
-	return c.cmds[k]
-}
-
-// GetSortedCmds returns sorted list of command names.
-func (c *CLI) GetSortedCmds() []string {
-	cmds := reflect.ValueOf(c.cmds).MapKeys()
-	scmds := make([]string, len(cmds))
-	for i, cmd := range cmds {
-		scmds[i] = cmd.String()
+// AddEnvVar returns pointer to a new environment variable that is required to run every command.
+// Method requires name, eg. MY_VAR, and description.
+func (c *CLI) AddEnvVar(n string, d string) {
+	c.envVars[n] = &param{
+		name:    n,
+		desc:    d,
+		flags:   IsRequired,
+		options: paramOptions{},
 	}
-	sort.Strings(scmds)
-	return scmds
-}
-
-// PrintHelp prints usage info to stdout file.
-func (c *CLI) PrintHelp() {
-	fmt.Fprintf(c.stdout, c.name+" by "+c.author+"\n"+c.desc+"\n\n")
-	fmt.Fprintf(c.stdout, "Usage: "+path.Base(os.Args[0])+" [FLAGS] COMMAND\n\n")
-
-	if len(c.envs) > 0 {
-		fmt.Fprintf(c.stdout, "Required environment variables:\n")
-		w := new(tabwriter.Writer)
-		w.Init(c.stdout, 8, 8, 0, '\t', 0)
-		for n, flg := range c.envs {
-			fmt.Fprintf(w, "%s\t%s\n", n, flg.desc)
-		}
-		w.Flush()
-	}
-
-	fmt.Fprintf(c.stdout, "Commands:\n")
-
-	w := new(tabwriter.Writer)
-	w.Init(c.stdout, 8, 8, 0, '\t', 0)
-	for _, n := range c.GetSortedCmds() {
-		cmd := c.GetCmd(n)
-		fmt.Fprintf(w, "  %s\t%s\n", n, cmd.desc)
-	}
-	w.Flush()
-
-	fmt.Fprintf(c.stdout, "\nRun '"+path.Base(os.Args[0])+" COMMAND --help' for more information on a command.\n")
-}
-
-// PrintInvalidCmd prints invalid command error to stderr file.
-func (c *CLI) PrintInvalidCmd(cmd string) {
-	fmt.Fprintf(c.stderr, "Invalid command: "+cmd+"\n\n")
-	c.PrintHelp()
-}
-
-// AddCmd creates a new command with name n, description d and handler of f. It creates instance of CLICmd, attaches it to CLI and returns it.
-func (c *CLI) AddCmd(n string, d string, f func(cli *CLI) int) *CLICmd {
-	cmd := NewCLICmd(n, d, f)
-	c.AttachCmd(cmd)
-	return cmd
-}
-
-// AddRequiredEnv create a new flag
-func (c *CLI) AddRequiredEnv(n string, d string, nf int32) {
-	flg := NewCLIFlag(n, "", "", d, nf, nil)
-	c.AttachEnv(flg)
-}
-
-// AddFlagToCmds adds a flag to all attached commands. It creates CLIFlag instance and attaches it.
-func (c *CLI) AddFlagToCmds(n string, a string, hv string, d string, nf int32, fn func(*CLICmd)) {
-	for _, nm := range c.GetSortedCmds() {
-		cmd := c.GetCmd(nm)
-		flg := NewCLIFlag(n, a, hv, d, nf, fn)
-		cmd.AttachFlag(flg)
-	}
-}
-
-// AddArg adds an argument to all attached commands.
-func (c *CLI) AddArgToCmds(n string, hv string, d string, nf int32) {
-	for _, nm := range c.GetSortedCmds() {
-		cmd := c.GetCmd(nm)
-		if cmd.argsIdx > 9 {
-			log.Fatal("Only 10 arguments are allowed")
-		}
-		arg := NewCLIFlag(n, "", hv, d, nf, nil)
-		cmd.AttachArg(arg)
-	}
-}
-
-// getFlagSetPtrs creates flagset instance, parses flags and returns list of pointers to results of parsing the flags.
-func (c *CLI) getFlagSetPtrs(cmd *CLICmd) (map[string]interface{}, map[string]interface{}, []string) {
-	fset := flag.NewFlagSet("flagset", flag.ContinueOnError)
-	// nothing should come out of flagset
-	fset.Usage = func() {}
-	fset.SetOutput(ioutil.Discard)
-
-	nptrs := make(map[string]interface{})
-	aptrs := make(map[string]interface{})
-	fs := cmd.GetSortedFlags()
-	for _, n := range fs {
-		f := cmd.GetFlag(n)
-		if f.IsRequireValue() {
-			nptrs[n] = fset.String(n, "", "")
-			aptrs[f.alias] = fset.String(f.alias, "", "")
-		} else if f.nflags&TypeBool > 0 {
-			nptrs[n] = fset.Bool(n, false, "")
-			aptrs[f.alias] = fset.Bool(f.alias, false, "")
-		}
-	}
-	fset.Parse(os.Args[2:])
-	return nptrs, aptrs, fset.Args()
-}
-
-// parseFlags iterates over flags and args and validates them. In case of error it prints out to CLI stderr.
-func (c *CLI) parseFlags(cmd *CLICmd) int {
-	// check required environment variables
-	if len(cmd.envs) > 0 {
-		for env, flg := range cmd.envs {
-			v := os.Getenv(env)
-			flg.nflags = flg.nflags | Required
-			err := flg.ValidateValue(false, v, "")
-			if err != nil {
-				fmt.Fprintf(c.stderr, "ERROR: "+err.Error()+"\n")
-				cmd.PrintHelp(c)
-				return 1
-			}
-		}
-	}
-
-	if c.parsedFlags == nil {
-		c.parsedFlags = make(map[string]string)
-	}
-
-	fs := cmd.GetSortedFlags()
-	nptrs, aptrs, args := c.getFlagSetPtrs(cmd)
-
-	for _, n := range fs {
-		f := cmd.GetFlag(n)
-		a := f.alias
-
-		var nv string
-		var av string
-		if f.nflags&TypeBool > 0 {
-			c.parsedFlags[n] = "false"
-			if *(nptrs[n]).(*bool) == true || *(aptrs[a]).(*bool) == true {
-				c.parsedFlags[n] = "true"
-				if f.fn != nil {
-					f.fn(cmd)
-				}
-			}
-			continue
-		}
-
-		nv = *(nptrs[n]).(*string)
-		av = *(aptrs[a]).(*string)
-
-		err := f.ValidateValue(false, nv, av)
-		if err != nil {
-			fmt.Fprintf(c.stderr, "ERROR: "+err.Error()+"\n")
-			cmd.PrintHelp(c)
-			return 1
-		}
-
-		c.parsedFlags[n] = av
-		if nv != "" {
-			c.parsedFlags[n] = nv
-		}
-	}
-
-	if c.parsedArgs == nil {
-		c.parsedArgs = make(map[string]string)
-	}
-
-	as := cmd.GetSortedArgs()
-
-	for i, n := range as {
-		v := ""
-		if len(args) >= i+1 {
-			v = args[i]
-		}
-
-		f := cmd.GetArg(n)
-
-		err := f.ValidateValue(true, v, "")
-		if err != nil {
-			fmt.Fprintf(c.stderr, "ERROR: "+err.Error()+"\n")
-			cmd.PrintHelp(c)
-			return 1
-		}
-
-		c.parsedArgs[n] = v
-	}
-
-	postv := cmd.GetPostValidation()
-	if postv != nil {
-		err := postv(c)
-		if err != nil {
-			fmt.Fprintf(c.stderr, "ERROR: "+err.Error()+"\n")
-			cmd.PrintHelp(c)
-			return 1
-		}
-	}
-	return 0
-}
-
-// SetStdin sets stdin
-func (c *CLI) SetStdin(stdin *os.File) {
-	c.stdin = stdin
-}
-
-// Run parses the arguments, validates them and executes command handler. In case of invalid arguments, error is printed to stderr and 1 is returned. Return value behaves like exit code.
-func (c *CLI) Run(stdout *os.File, stderr *os.File) int {
-	c.stdout = stdout
-	c.stderr = stderr
-	// display help
-	if len(os.Args[1:]) < 1 || (len(os.Args[1:]) == 1 && (os.Args[1] == "-h" || os.Args[1] == "--help")) {
-		c.PrintHelp()
-		return 0
-	}
-	for _, n := range c.GetSortedCmds() {
-		if n == os.Args[1] {
-			// display command help
-			if len(os.Args[1:]) == 2 && (os.Args[2] == "-h" || os.Args[2] == "--help") {
-				c.GetCmd(n).PrintHelp(c)
-				return 0
-			}
-
-			// check required environment variables
-			if len(c.envs) > 0 {
-				for env, flg := range c.envs {
-					v := os.Getenv(env)
-					flg.nflags = flg.nflags | Required
-					err := flg.ValidateValue(false, v, "")
-					if err != nil {
-						fmt.Fprintf(c.stderr, "ERROR: "+err.Error()+"\n")
-						c.PrintHelp()
-						return 1
-					}
-				}
-			}
-
-			exitCode := c.parseFlags(c.GetCmd(n))
-			if exitCode > 0 {
-				return exitCode
-			}
-			return c.GetCmd(n).Run(c)
-		}
-	}
-	// command not found
-	c.PrintInvalidCmd(os.Args[1])
-	return 1
 }
 
 // Flag returns value of flag.
@@ -302,8 +80,267 @@ func (c *CLI) Arg(n string) string {
 	return c.parsedArgs[n]
 }
 
-// NewCLI creates new instance of CLI with name n, description d and author a and returns it.
-func NewCLI(n string, d string, a string) *CLI {
-	c := &CLI{name: n, desc: d, author: a}
-	return c
+// Run parses the arguments, validates them and executes command handler.
+// In case of invalid arguments, error is printed to stderr and 1 is returned.  Return value should be treated as exit
+// code.
+func (c *CLI) Run() int {
+	// display help, first arg is binary filename
+	if len(os.Args) < 2 || os.Args[1] == "-h" || os.Args[1] == "--help" {
+		c.printHelp()
+		return 0
+	}
+	for _, n := range c.sortedCmds() {
+		if n != os.Args[1] {
+			continue
+		}
+		// display command help
+		if len(os.Args) > 2 && (os.Args[2] == "-h" || os.Args[2] == "--help") {
+			c.cmds[n].printHelp()
+			return 0
+		}
+
+		// check required environment variables
+		if len(c.envVars) > 0 {
+			for env, param := range c.envVars {
+				v := os.Getenv(env)
+				param.flags = param.flags | IsRequired
+				err := param.validateValue(v)
+				if err != nil {
+					fmt.Fprintf(os.Stderr, "ERROR: %s %s: %s\n", c.getParamTypeName(ParamEnvVar), param.name, err.Error())
+					c.printHelp()
+					return 1
+				}
+			}
+		}
+
+		// parse and validate all the flags and args
+		exitCode := c.parseFlags(c.cmds[n])
+		if exitCode > 0 {
+			return exitCode
+		}
+
+		return c.cmds[n].handler(c)
+	}
+
+	// command not found
+	c.printInvalidCmd(os.Args[1])
+	return 1
+}
+
+func (c *CLI) sortedCmds() []string {
+	cmds := reflect.ValueOf(c.cmds).MapKeys()
+	scmds := make([]string, len(cmds))
+	for i, cmd := range cmds {
+		scmds[i] = cmd.String()
+	}
+	sort.Strings(scmds)
+	return scmds
+}
+
+func (c *CLI) sortedEnvVars() []string {
+	evs := reflect.ValueOf(c.envVars).MapKeys()
+	sevs := make([]string, len(evs))
+	for i, ev := range evs {
+		sevs[i] = ev.String()
+	}
+	sort.Strings(sevs)
+	return sevs
+}
+
+func (c *CLI) printHelp() {
+	fmt.Fprintf(os.Stdout, "%s by %s\n%s\n\n", c.name, c.author, c.desc)
+	fmt.Fprintf(os.Stdout, "Usage: %s COMMAND\n\n", path.Base(os.Args[0]))
+
+	if len(c.envVars) > 0 {
+		fmt.Fprintf(os.Stdout, "Required environment variables:\n")
+		w := new(tabwriter.Writer)
+		w.Init(os.Stdout, 8, 8, 0, '\t', 0)
+		for _, n := range c.sortedEnvVars() {
+			fmt.Fprintf(w, "%s\t%s\n", n, c.envVars[n].desc)
+		}
+		w.Flush()
+	}
+
+	fmt.Fprintf(os.Stdout, "Commands:\n")
+	w := new(tabwriter.Writer)
+	w.Init(os.Stdout, 10, 8, 0, '\t', 0)
+	for _, n := range c.sortedCmds() {
+		fmt.Fprintf(w, "  %s\t%s\n", n, c.cmds[n].desc)
+	}
+	w.Flush()
+
+	fmt.Fprintf(os.Stdout, "\nRun '%s COMMAND --help' for command syntax.\n", path.Base(os.Args[0]))
+}
+
+func (c *CLI) printInvalidCmd(cmd string) {
+	fmt.Fprintf(os.Stderr, "Invalid command: %s\n\n", cmd)
+	c.printHelp()
+}
+
+// getFlagSetPtrs creates flagset instance, parses flags and returns list of pointers to results of parsing the flags.
+func (c *CLI) getFlagSetPtrs(cmd *Cmd) (map[string]interface{}, map[string]interface{}, []string) {
+	fset := flag.NewFlagSet("flagset", flag.ContinueOnError)
+	// nothing should come out of flagset
+	fset.Usage = func() {}
+	fset.SetOutput(ioutil.Discard)
+
+	nameFlags := make(map[string]interface{})
+	aliasFlags := make(map[string]interface{})
+	fs := cmd.sortedFlags()
+	for _, n := range fs {
+		f := cmd.flags[n]
+		if f.valueType == TypeBool {
+			nameFlags[n] = fset.Bool(n, false, "")
+			aliasFlags[f.alias] = fset.Bool(f.alias, false, "")
+		} else {
+			nameFlags[n] = fset.String(n, "", "")
+			aliasFlags[f.alias] = fset.String(f.alias, "", "")
+		}
+	}
+	fset.Parse(os.Args[2:])
+	return nameFlags, aliasFlags, fset.Args()
+}
+
+func (c *CLI) checkEnvVars(cmd *Cmd) int {
+	if len(cmd.envVars) == 0 {
+		return 0
+	}
+
+	for env, envVar := range cmd.envVars {
+		v := os.Getenv(env)
+		envVar.flags = envVar.flags | IsRequired
+		err := envVar.validateValue(v)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "ERROR: %s %s: %s\n", c.getParamTypeName(ParamEnvVar), envVar.name, err.Error())
+			cmd.printHelp()
+			return 1
+		}
+	}
+
+	return 0
+}
+
+func (c *CLI) processOnTrue(cmd *Cmd, fs []string, nflags map[string]interface{}, aflags map[string]interface{}) {
+	for _, name := range fs {
+		if cmd.flags[name].valueType != TypeBool {
+			continue
+		}
+
+		if cmd.flags[name].options.onTrue == nil {
+			continue
+		}
+
+		c.parsedFlags[name] = "false"
+		if *(nflags[name]).(*bool) == true || *(aflags[cmd.flags[name].alias]).(*bool) == true {
+			c.parsedFlags[name] = "true"
+			cmd.flags[name].options.onTrue(cmd)
+		}
+	}
+}
+
+func (c *CLI) processFlags(cmd *Cmd, fs []string, nflags map[string]interface{}, aflags map[string]interface{}) int {
+	for _, name := range fs {
+		flag := cmd.flags[name]
+
+		if flag.valueType == TypeBool {
+			continue
+		}
+
+		aliasValue := *(aflags[flag.alias]).(*string)
+		nameValue := *(nflags[name]).(*string)
+		if nameValue != "" && aliasValue != "" {
+			fmt.Fprintf(os.Stderr, "ERROR: Both -%s and --%s passed", flag.alias, flag.name)
+			return 1
+		}
+		v := aliasValue
+		if nameValue != "" {
+			v = nameValue
+		}
+
+		err := flag.validateValue(v)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "ERROR: %s %s: %s\n", c.getParamTypeName(ParamFlag), name, err.Error())
+			cmd.printHelp()
+			return 1
+		}
+
+		c.parsedFlags[name] = v
+	}
+
+	return 0
+}
+
+func (c *CLI) processArgs(cmd *Cmd, as []string, args []string) int {
+	for i, n := range as {
+		v := ""
+		if len(args) >= i+1 {
+			v = args[i]
+		}
+
+		err := cmd.args[n].validateValue(v)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "ERROR: %s %s: %s\n", c.getParamTypeName(ParamArg), cmd.args[n].helpValue, err.Error())
+			cmd.printHelp()
+			return 1
+		}
+
+		c.parsedArgs[n] = v
+	}
+
+	return 0
+}
+
+func (c *CLI) processOnPostValidation(cmd *Cmd) int {
+	if cmd.options.onPostValidation == nil {
+		return 0
+	}
+
+	err := cmd.options.onPostValidation(cmd)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "ERROR: %s\n", err.Error())
+		cmd.printHelp()
+		return 1
+	}
+
+	return 0
+}
+
+func (c *CLI) parseFlags(cmd *Cmd) int {
+	// check required environment variables
+	if exitCode := c.checkEnvVars(cmd); exitCode != 0 {
+		return exitCode
+	}
+
+	fs := cmd.sortedFlags()
+	nameFlags, aliasFlags, args := c.getFlagSetPtrs(cmd)
+
+	// Loop through boolean flags and execute onTrue() hook if exists.  That function might be used to change behaviour
+	// of other flags, eg. when -e is added, another flag or argument might become required (or obsolete).
+	// Bool fields will be parsed out in this loop so no reason to process them again in the next one.
+	c.processOnTrue(cmd, fs, nameFlags, aliasFlags)
+
+	if exitCode := c.processFlags(cmd, fs, nameFlags, aliasFlags); exitCode != 0 {
+		return exitCode
+	}
+
+	as := cmd.sortedArgs()
+	if exitCode := c.processArgs(cmd, as, args); exitCode != 0 {
+		return exitCode
+	}
+
+	if exitCode := c.processOnPostValidation(cmd); exitCode != 0 {
+		return exitCode
+	}
+
+	return 0
+}
+
+func (c *CLI) getParamTypeName(t int8) string {
+	if t == ParamArg {
+		return "Argument"
+	}
+	if t == ParamEnvVar {
+		return "Env var"
+	}
+	return "Flag"
 }
--- cli_test.go
+++ cli_test.go
@@ -1,105 +1,88 @@
 package broccli
 
 import (
+	"fmt"
+	"io"
+	"log"
 	"os"
+	"strings"
 	"testing"
 )
 
-func h(c *CLI) int {
-	return 0
-}
-
-func createCLI() *CLI {
-	c := NewCLI("Example CLI", "Silly app", "Author <a@example.com>")
-
-	cmd1 := c.AddCmd("command", "Prints out something", h)
-	cmd1.AddFlag("bool", "b", "", "Boolean flag", TypeBool, nil)
-	cmd1.AddFlag("input", "i", "filepath", "Path to a file", TypePathFile|Required, nil)
-	cmd1.AddFlag("title", "t", "title", "Title of the project", TypeString|Required, nil)
-	cmd1.AddFlag("desc", "d", "description", "Description of the project", TypeString, nil)
-
-	cmd2 := c.AddCmd("anotherone", "Initialises project", h)
-	cmd2.AddFlag("int", "i", "int", "Integer flag", TypeInt|Required, nil)
-	cmd2.AddFlag("float", "f", "float", "Float flag", TypeFloat|Required, nil)
-	cmd2.AddFlag("anum", "", "alphanumeric", "Alphanumeric flag", TypeAlphanumeric|Required, nil)
-
-	cmd3 := c.AddCmd("three", "Third command that does something", h)
-	cmd3.AddFlag("many-ints", "i", "int,int,...", "Many integers comma-delimetered", TypeInt|AllowMany, nil)
-	cmd3.AddFlag("many-floats", "f", "float;float;...", "Many floats semicolon-delimetered", TypeFloat|AllowMany|ManySeparatorSemiColon|Required, nil)
-	cmd3.AddFlag("many-anums", "a", "anum:anum:...", "Many alphanumeric colon-delimetered", TypeAlphanumeric|AllowMany|ManySeparatorColon, nil)
-	cmd3.AddFlag("more", "m", "alphanumeric+dot+uscore", "Alphanumeric flag with additional dots and underscore allowed", TypeAlphanumeric|AllowDots|AllowUnderscore|Required, nil)
-
-	cmd4 := c.AddCmd("play", "Play the game on a specific map", h)
-	cmd4.AddFlag("level", "l", "1", "Starting level (1-50)", TypeInt|Required, nil)
-	cmd4.AddFlag("difficulty", "d", "3", "Difficulty (1-5)", TypeInt, nil)
-	cmd4.AddArg("map", "MAP", "Name of the map, eg. arena", TypeString|Required)
-	cmd4.AddArg("opponents", "OPPONENTS", "Number of oponents, default 5", TypeInt|Required)
-	cmd4.AddArg("foes", "FOES", "Number of foes, default 0", TypeInt)
-
-	cmd5 := c.AddCmd("overwrite_arg", "Overwrite argument when flag is passed", h)
-	cmd5.AddFlag("overwrite", "o", "", "Make argument not required", TypeBool, func(c *CLICmd) {
-		f := c.GetArg("notrequired")
-		f.nflags = TypeString
-	})
-	cmd5.AddArg("notrequired", "NOTREQUIRED", "Argument required when no -o", TypeString|Required)
-
-	c.AddFlagToCmds("all", "x", "", "Flag added to all commands", TypeInt, nil)
-	c.AddArgToCmds("all", "ALL", "Arg added to all commands", TypeString)
-
-	return c
-}
-
-func assertExitCode(t *testing.T, cli *CLI, a []string, c int) {
-	os.Args = a
-	f, _ := os.Open("/dev/null")
-	defer f.Close()
-	got := cli.Run(f, f)
-	want := c
-	if got != want {
-		t.Errorf("got %d want %d\n", got, want)
+// TestCLI creates a test CLI instance with a single command with flags and test
+// basic functionality.
+func TestCLI(t *testing.T) {
+	f, err := os.CreateTemp("", "stdout")
+	if err != nil {
+		log.Fatal(err)
 	}
-}
+	defer os.Remove(f.Name())
 
-func TestFlags(t *testing.T) {
-	c := createCLI()
+	devNull, err := os.OpenFile("/dev/null", os.O_APPEND, 644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	os.Stdout = devNull
+	os.Stderr = devNull
 
-	t.Run("exit with code 0 when no flags or help", func(t *testing.T) {
-		assertExitCode(t, c, []string{"test"}, 0)
-		assertExitCode(t, c, []string{"test", "-h"}, 0)
-		assertExitCode(t, c, []string{"test", "--help"}, 0)
-		assertExitCode(t, c, []string{"test", "command", "-h"}, 0)
-		assertExitCode(t, c, []string{"test", "command", "--help"}, 0)
+	c := NewCLI("Example", "App", "Author <a@example.com>")
+	cmd1 := c.AddCmd("cmd1", "Prints out a string", func(c *CLI) int {
+		fmt.Fprintf(f, "TESTVALUE:%s%s\n\n", c.Flag("tekst"), c.Flag("alphanumdots"))
+		return 2
 	})
+	cmd1.AddFlag("tekst", "t", "Text", "Text to print", TypeString, IsRequired)
+	cmd1.AddFlag("alphanumdots", "a", "Alphanum with dots", "Can have dots", TypeAlphanumeric, AllowDots)
+	cmd1.AddFlag("make-required", "r", "", "Make alphanumdots required", TypeBool, 0, OnTrue(func(c *Cmd) {
+		c.flags["alphanumdots"].flags = c.flags["alphanumdots"].flags | IsRequired
+	}))
 
-	t.Run("exit with code 1 when required flag is missing", func(t *testing.T) {
-		assertExitCode(t, c, []string{"test", "command", "-b", "-t", "title", "-d", "desc"}, 1)
-		assertExitCode(t, c, []string{"test", "command", "-i", "cli_test.go"}, 1)
-		assertExitCode(t, c, []string{"test", "anotherone"}, 1)
-		assertExitCode(t, c, []string{"test", "three", "-i", "1,2,3"}, 1)
-	})
+	os.Args = []string{"test", "cmd1"}
+	got := c.Run()
+	if got != 1 {
+		t.Errorf("CLI.Run() should have returned 1 instead of %d", got)
+	}
 
-	t.Run("exit with code 1 when value is invalid", func(t *testing.T) {
-		assertExitCode(t, c, []string{"test", "command", "-i", "nonexistingfile", "-t", "title"}, 1)
-		assertExitCode(t, c, []string{"test", "anotherone", "--int", "aaaa", "--float", "123.12", "--anum", "validvalue"}, 1)
-		assertExitCode(t, c, []string{"test", "anotherone", "--int", "123", "--float", "123", "--anum", "validvalue"}, 1)
-		assertExitCode(t, c, []string{"test", "anotherone", "--int", "123", "--float", "123.12", "--anum", "^^4443####"}, 1)
-		assertExitCode(t, c, []string{"test", "three", "-i", "aasd,asda", "-f", "12.33", "-a", "user1", "-m", "user.1"}, 1)
-		assertExitCode(t, c, []string{"test", "three", "-i", "1,2,3", "-f", "12,33", "-a", "user1", "-m", "user.1"}, 1)
-		assertExitCode(t, c, []string{"test", "three", "-i", "1,2,3", "-f", "23.23,24.24,25.25", "-a", "user1.", "-m", "user_1"}, 1)
-	})
+	os.Args = []string{"test", "cmd1", "-t", ""}
+	got = c.Run()
+	if got != 1 {
+		t.Errorf("CLI.Run() should have returned 1 instead of %d", got)
+	}
 
-	t.Run("exit with code 1 when arg is missing", func(t *testing.T) {
-		assertExitCode(t, c, []string{"test", "play", "--level", "1", "-d", "4"}, 1)
-		assertExitCode(t, c, []string{"test", "play", "map", "4", "5", "--level", "1", "-d", "4"}, 1)
-		assertExitCode(t, c, []string{"test", "play", "-l", "1", "-d", "4", "winter"}, 1)
-	})
+	os.Args = []string{"test", "cmd1", "--tekst", "Tekst123", "--alphanumdots"}
+	got = c.Run()
+	if got != 2 {
+		t.Errorf("CLI.Run() should have returned 2 instead of %d", got)
+	}
 
-	t.Run("exit with code 1 when arg has invalid value", func(t *testing.T) {
-		assertExitCode(t, c, []string{"test", "play", "-l", "1", "-d", "4", "winter", "five"}, 1)
-	})
+	os.Args = []string{"test", "cmd1", "--tekst", "Tekst123", "-r"}
+	got = c.Run()
+	if got != 1 {
+		t.Errorf("CLI.Run() should have returned 1 instead of %d", got)
+	}
 
-	t.Run("exit with code 0 when required arg is no longer required because it was overwritten by a flag", func(t *testing.T) {
-		assertExitCode(t, c, []string{"test", "overwrite_arg", "REQUIRED_ARG_HERE"}, 0)
-		assertExitCode(t, c, []string{"test", "overwrite_arg", "-o"}, 0)
-	})
+	os.Args = []string{"test", "cmd1", "--tekst", "Tekst123", "--alphanumdots", "aZ0-9"}
+	got = c.Run()
+	if got != 1 {
+		t.Errorf("CLI.Run() should have returned 1 instead of %d", got)
+	}
+
+	os.Args = []string{"test", "cmd1", "--tekst", "Tekst123", "--alphanumdots", "aZ0.9"}
+	got = c.Run()
+	if got != 2 {
+		t.Errorf("CLI.Run() should have returned 2 instead of %d", got)
+	}
+
+	f2, err := os.Open(f.Name())
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f2.Close()
+	b, err := io.ReadAll(f2)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if !strings.Contains(string(b), "TESTVALUE:Tekst123aZ0.9") {
+		t.Errorf("Cmd handler failed to work")
+	}
 }
